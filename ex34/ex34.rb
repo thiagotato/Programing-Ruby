5f8d8697eeb01e9b72d2d86e40f2241348bbcd1c
Thiago Tato <thiagotato@bemobi.com.br>
2017-05-22 10:30:48 -0300

    add ex34

---
+++ ex34/ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1: #{animals[1]}"
+puts "The third animal: #{animals[2]}"
+puts "The first animal: #{animals[0]}"
+puts "The animal at 3: #{animals[3]}"
+puts "The fifth animal: #{animals[4]}"
+puts "The animal at 2: #{animals[2]}"
+puts "The sixth animal: #{animals[5]}"
+puts "The animal at 4: #{animals[4]}"
