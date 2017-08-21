c745dd6d2e0aa60f9e3bf5b1b6f29788e0b42b95
Thiago Tato <thiagotato@bemobi.com.br>
2017-08-21 13:48:50 -0300

    update

--- ex45/ex45.rb
+++ ex45/ex45.rb
@@ -9,20 +9,33 @@ end
 
 class Cards < Deck
   def card
+    @cd = []
     @nipes.each { |nip|
       @numbers.each { |numb|
-        puts nip + numb
+        game = nip + numb
+        @cd.push(game)
       }  
     }
+    return @cd 
   end
 end
 
 class Engine
+  def game 
+    start = Cards.new
+    take = Random.new
+    game = start.card
+    return game[take.rand(52)] 
+  end
+
+  # player na primeira jogada deve receber duas cartas que devem ser removidas da pilha para não serem entregues novamente. Deve ser feito um calculo com as duas cartas para verficar se o player tem 21 e retornar em caso de vitoria ou derota. Player pode pegar mais cartas para chegar mais perto de 21.
+
 end
 
 class Player
 end
 
-start = Cards.new
+ok = Engine.new
+op = ok.game
 
-start.card
+puts op
